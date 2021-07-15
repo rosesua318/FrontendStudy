cc0fb2283faee80c4aa20b09c2499e2fd46ccb67
rosesua318 <rosesua@duksung.ac.kr>
2021-07-15 21:52:23 +0900

    Swiper 2

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -55,3 +55,36 @@ new Swiper(".notice-line .swiper-container", {
   autoplay: true, // 자동재생 여부
   loop: true, // 반복재생 여부
 });
+
+new Swiper(".promotion .swiper-container", {
+  direction: "horizontal",
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true, // 반복재생 여부
+  autoplay: {
+    delay: 5000, // 5초에 한번씩 슬라이드됨
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev", // 이전버튼 요소 선택자
+    nextEl: ".promotion .swiper-next", // 다음버튼 요소 선택자
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", () => {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add("hide");
+  } else {
+    // 보임 처리
+    promotionEl.classList.remove("hide");
+  }
+});
